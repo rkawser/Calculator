5531c45e4f4548dce0f54563628b3f6fb74db496
raihan kawser <raihankawser55@gmail.com>
2022-09-25 15:30:21 +0600

    commit update

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ Array.from(button).forEach(function(value){
         string =eval(string)
         input.value=string
     }
-    
+
    else if(e.target.innerHTML === 'C'){
         string =''
         input.value=string
     }
-   
+    else if(e.target.innerHTML === '☾'){
+        string =''
+        input.value=string
+    }
     else{
-        console.log(e.target)
         string+= e.target.innerHTML;
         input.value=string
     }
       
    })
 })
+
+
+// apply dark mood
+
+
+let dark = document.getElementById('dark')
+
+dark.onclick=function(){
+    document.body.classList.toggle('dark-them')
+}
